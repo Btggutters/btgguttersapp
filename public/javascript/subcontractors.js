bb587b0232ae018db3c6bbf3f2a656401641adc5
Noisemaker111 <jk10192000@gmail.com>
2023-11-19 21:12:22 -0500

    subcontractor up

--- public/javascript/subcontractors.js
+++ public/javascript/subcontractors.js
@@ -1,68 +1,91 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("addSubcontractorButton");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 document.getElementById("subcontractorForm").onsubmit = function(e) {
     e.preventDefault();
   
     // Get the form data
+    var modal = document.getElementById("myModal");
     var companyName = document.getElementById("companyName").value;
     var salesName = document.getElementById("salesName").value;
     var salesNumber = document.getElementById("salesNumber").value;
     var salesEmail = document.getElementById("salesEmail").value;
     var companyStreetAddress = document.getElementById("companyStreetAddress").value;
+    var companyPricePerFoot = document.getElementById("companyPricePerFoot").value;
   
-    // Create a new subcontractor card
-    var card = document.createElement("div");
-    card.className = "subcontractorCard";
-  
-    // Add the company name
-    var companyNameElement = document.createElement("div");
-    companyNameElement.className = "subcontractorRow";
-    companyNameElement.innerText = companyName;
-    card.appendChild(companyNameElement);
-  
-    // Add the sales name
-    var salesNameElement = document.createElement("div");
-    salesNameElement.className = "subcontractorRow";
-    salesNameElement.innerText = salesName;
-    card.appendChild(salesNameElement);
-  
-    // Add the sales number
-    var salesNumberElement = document.createElement("div");
-    salesNumberElement.className = "subcontractorRow";
-    salesNumberElement.innerText = salesNumber;
-    card.appendChild(salesNumberElement);
-  
-    // Add the sales email
-    var salesEmailElement = document.createElement("div");
-    salesEmailElement.className = "subcontractorRow";
-    salesEmailElement.innerText = salesEmail;
-    card.appendChild(salesEmailElement);
-  
-    // Add the company street address
-    var companyStreetAddressElement = document.createElement("div");
-    companyStreetAddressElement.className = "subcontractorRow";
-    companyStreetAddressElement.innerText = companyStreetAddress;
-    card.appendChild(companyStreetAddressElement);
-  
-    // Add the new card to the main element
-    document.querySelector("main").appendChild(card);
+    // Send a POST request to the server
+    fetch('/add-subcontractor', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        companyName: companyName,
+        salesName: salesName,
+        salesNumber: salesNumber,
+        salesEmail: salesEmail,
+        companyStreetAddress: companyStreetAddress,
+        companyPricePerFoot: companyPricePerFoot,
+      }),
+    })
+    .then(response => {
+        console.log(response);
+        return response.json();
+      })
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   
     // Close the modal
     modal.style.display = "none";
-}
+  }
+  
+  document.getElementById("fakeSubmit").onclick = function() {
+    // Fill the form with fake data
+    document.getElementById("companyName").value = "Fake Company";
+    document.getElementById("salesName").value = "Fake Sales Name";
+    document.getElementById("salesNumber").value = "1234567890";
+    document.getElementById("salesEmail").value = "fake@company.com";
+    document.getElementById("companyStreetAddress").value = "123 Fake St";
+    // Make sure this field exists in your form
+    document.getElementById("companyPricePerFoot").value = 7;
+};
+window.onload = function() {
+    // Send a GET request to the server
+    fetch('/get-subcontractors')
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      console.log('Success:', data);
+      // Create subcontractorCards for each subcontractor
+      var subcontractorContainer = document.querySelector('.subcontractorContainer');
+      data.forEach(subcontractor => {
+        var subcontractorCard = document.createElement('div');
+        subcontractorCard.className = 'subcontractorCard';
+        subcontractorCard.innerHTML = `
+          <div class="subcontractorRow">
+            <span>${subcontractor.company_name}</span>
+          </div>
+          <div class="subcontractorRow">
+            <span>${subcontractor.sales_name}</span>
+            <span class="subcontractCompanyPricePerFoot">${subcontractor.company_price_per_foot}</span>
+          </div>
+          <div class="subcontractorRow">
+            <span>${subcontractor.sales_number}</span>
+          </div>
+          <div class="subcontractorRow">
+            <span>${subcontractor.sales_email}</span>
+          </div>
+          <div class="subcontractorRow">
+            <span>${subcontractor.company_street_address}</span>
+          </div>
+        `;
+        subcontractorContainer.appendChild(subcontractorCard);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port = 3000;
 const path = require('path');
 const { Pool } = require('pg');
 
+const cors = require('cors');
+app.use(cors());
+
 const pool = new Pool({
   user: 'neon',
   host: 'ep-dawn-dew-50528787.us-east-2.aws.neon.tech',
@@ -15,11 +18,7 @@ const pool = new Pool({
   }
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-
+app.use(express.json());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,25 +27,46 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/pages/home.html'));
 });
 
+app.post('/add-subcontractor', (req, res) => {
+  // Get the form data from the request body
+  var { companyName, salesName, salesNumber, salesEmail, companyStreetAddress, companyPricePerFoot } = req.body;
+
+  // Create an INSERT INTO query
+  var insertQuery = `
+    INSERT INTO subcontractors (company_name, sales_name, sales_number, sales_email, company_street_address, company_price_per_foot)
+    VALUES ($1, $2, $3, $4, $5, $6)
+  `;
+
+  // Execute the query
+  pool.query(insertQuery, [companyName, salesName, salesNumber, salesEmail, companyStreetAddress, companyPricePerFoot], (err, result) => {
+    if (err) {
+      console.log('Error executing query', err.stack);
+      res.status(500).json({ error: err.stack });
+    } else {
+      console.log('Data inserted');
+      res.status(200).json({ message: 'Data inserted' });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS subcontractors (
-    id SERIAL PRIMARY KEY,
-    company_name VARCHAR(255),
-    sales_name VARCHAR(255),
-    sales_number VARCHAR(255),
-    sales_email VARCHAR(255),
-    company_street_address VARCHAR(255)
-  )
-`;
+app.get('/get-subcontractors', (req, res) => {
+  // Create a SELECT query
+  var selectQuery = `
+    SELECT * FROM subcontractors
+  `;
 
-pool.query(createTableQuery, (err, res) => {
-  if (err) {
-    console.log('Error executing query', err.stack);
-  } else {
-    console.log('Table created');
-  }
+  // Execute the query
+  pool.query(selectQuery, (err, result) => {
+    if (err) {
+      console.log('Error executing query', err.stack);
+      res.status(500).json({ error: err.stack });
+    } else {
+      console.log('Data retrieved');
+      res.status(200).json(result.rows);
+    }
+  });
 });
